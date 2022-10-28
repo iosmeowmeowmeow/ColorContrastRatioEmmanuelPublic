860f4ee8dd20281f8e18b4bc94a2310a3ede1197
Emmanuel Tugado <emmanuel.t@homeofpmg.com>
2022-10-28 13:03:08 +1100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ColorContrastRatioEmmanuel
+
+A description of this package.
---
+++ Sources/ColorContrastRatioEmmanuel/ColorContrastRatioEmmanuel.swift
@@ -0,0 +1,6 @@
+public struct ColorContrastRatioEmmanuel {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
